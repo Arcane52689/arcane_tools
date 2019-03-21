3259f167c5ea4941a5fa4b63632e7269bb8ed5b3
Thomas Jenkins <tjenkins52689@gmail.com>
2019-03-20 22:45:10 -0400

    Fix the bug past an hour

--- app/javascript/Utils/Format/Number.js
+++ app/javascript/Utils/Format/Number.js
@@ -1,14 +1,15 @@
 
 
-export function timePassed(seconds) {
-  if (seconds < 60) {
-    return `${Math.floor(seconds * 10) / 10} seconds`
-  } else if (seconds < 3600) {
-    return `${Math.floor(seconds / 60)} minutes ${Math.floor(seconds) % 60} seconds`
+export function timePassed(secondsPassed) {
+  if (secondsPassed < 60) {
+    return `${Math.floor(secondsPassed * 10) / 10} seconds`
+  } else if (secondsPassed < 3600) {
+    return `${Math.floor(secondsPassed / 60)} minutes ${Math.floor(secondsPassed) % 60} secondsPassed`
   } else {
-    let hours = Math.floor(seconds / 3600);
-    let minutes = Math.floor((seconds % 3600) / 60);
-    let seconds = Math.floor(seconds % 60);
+    let hours = Math.floor(secondsPassed / 3600);
+    let minutes = Math.floor((secondsPassed % 3600) / 60);
+    let seconds = Math.floor(secondsPassed % 60);
+
     return `${hours} hours, ${minutes} minutes, ${seconds} seconds`
   }
 }
